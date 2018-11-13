e7606689e867b70380ba96bd042c1f1393da7008
Julian Klein <jg.klein@protonmail.com>
2018-11-13 08:56:50 +0100

    First 4 sections finished.

--- scripts/lavskrika_rep_success_analysis.R
+++ scripts/lavskrika_rep_success_analysis.R
@@ -1,25 +1,61 @@
 ## Describe the purpose of the script
 ## 
 
-## First edit: 
-## Last edit: 
+## First edit: 20180602
+## Last edit: 20181113
 
-## Author: Julian Klein, jg.klein@protonmail.com
+## Author: Julian Klein
 
-## Clear environment and load libraries ----------------------------------------
+## 1. Clear environment and load libraries -------------------------------------
 
 rm(list = ls())
 
-library()
-library()
+library(ggplot2)
+library(data.table)
+library(reshape2)
+library(nlme)
+library(lme4)
+library(MuMIn)
+library(blmeco)
+library(DHARMa)
+library(pscl)
+library(boot)
+library(ltm)
+require(lmerTest)
 
-## Define or source functions used in this script ------------------------------
+## 2. Define or source functions used in this script ---------------------------
 
 source()
 
-## Load and explore data -------------------------------------------------------
+## 3. Load and explore data ----------------------------------------------------
 
-dir()
-#...
+dir("data")
 
-## -----------------------------------------------------------------------------
+ALS <- read.csv("data/ALS_rep_succ.csv")
+nest_height <- na.omit(read.csv("data/nest_heights.csv"))
+nest <- read.csv("data/nest_reproduction.csv")
+
+head(ALS)
+head(nest_heights)
+head(nest)
+
+## 4. Merge and prepare data set for analysis ----------------------------------
+
+## Exlcude experimental data from nest and add rep success yes/no because nest 
+## predation by corvids is absolute :
+nest <- nest[nest$excl_rep_suc == "no", ]
+nest$rep_succ <- ifelse(nest$fledl > 0, 1, 0)
+
+## Merge reproduction with ALS and nest height data:
+nest_ALS <- merge(nest, ALS, by = c("name", "territory", "year"))
+nest_height <- merge(nest, nest_height, all.x = TRUE, by = "name")
+
+## Make factors:
+nest_ALS$female_ring <- as.factor(nest_ALS$female_ring)
+nest_ALS$male_ring <- as.factor(nest_ALS$male_ring)
+nest_ALS$year <- as.factor(nest_ALS$year)
+
+## 5. Make models for different dts categorisation -----------------------------
+
+
+## -------------------------------END-------------------------------------------
